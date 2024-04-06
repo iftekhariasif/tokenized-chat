e955195449bb2e97706596d0bab38dc7c0ea8a68
IFTEKHAR I ASIF <40823261+iftekhariasif@users.noreply.github.com>
2024-04-07 04:36:13 +0900

    Added Auth Controller

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from '../services/auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body('nickname') nickname: string) {
+    const user = await this.authService.validateUser(nickname);
+    if (!user) {
+      return 'User not found';
+    }
+    return this.authService.login(user);
+  }
+}
