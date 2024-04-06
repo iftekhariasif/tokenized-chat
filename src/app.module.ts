6636fe8a46275249af61d17218551787b7122b03
IFTEKHAR I ASIF <40823261+iftekhariasif@users.noreply.github.com>
2024-04-07 02:08:17 +0900

    Registered Chat Gateway in the Application Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { MessagesService } from './services/messages.service';
 import { UsersController } from './controllers/users.controller';
 import { ChatRoomsController } from './controllers/chat-rooms.controller';
 import { MessagesController } from './controllers/messages.controller';
+import { ChatGateway } from './gateways/chat.gateway';
 
 @Module({
   imports: [
@@ -31,7 +32,18 @@ import { MessagesController } from './controllers/messages.controller';
     ChatRoomsModule,
     MessagesModule,
   ],
-  controllers: [AppController, UsersController, ChatRoomsController, MessagesController],
-  providers: [AppService, UsersService, ChatRoomsService, MessagesService],
+  controllers: [
+    AppController,
+    UsersController,
+    ChatRoomsController,
+    MessagesController,
+  ],
+  providers: [
+    AppService,
+    UsersService,
+    ChatRoomsService,
+    MessagesService,
+    ChatGateway,
+  ],
 })
 export class AppModule {}
