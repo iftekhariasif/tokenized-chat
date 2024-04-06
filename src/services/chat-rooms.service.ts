ac6561ef34602c091451e80ea00ef882e983c8f4
IFTEKHAR I ASIF <40823261+iftekhariasif@users.noreply.github.com>
2024-04-07 05:38:22 +0900

    Added CRUD Operation in ChatRoom Service

--- src/services/chat-rooms.service.ts
+++ src/services/chat-rooms.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ChatRoom } from '../models/chat-room';
 
 @Injectable()
-export class ChatRoomsService {}
+export class ChatRoomsService {
+  constructor(
+    @InjectRepository(ChatRoom)
+    private readonly chatRoomRepository: Repository<ChatRoom>,
+  ) {}
+
+  async createChatRoom(name: string, userId: number): Promise<ChatRoom> {
+    const newChatRoom = this.chatRoomRepository.create({
+      name,
+      creator: { id: userId },
+    });
+    await this.chatRoomRepository.save(newChatRoom);
+    return newChatRoom;
+  }
+
+  async getAllChatRooms(): Promise<ChatRoom[]> {
+    return this.chatRoomRepository.find({ relations: ['creator', 'messages'] });
+  }
+
+  async getChatRoomById(id: number): Promise<ChatRoom> {
+    const chatRoom = await this.chatRoomRepository.findOne({
+      where: { id },
+      relations: ['creator', 'messages'],
+    });
+    if (!chatRoom) {
+      throw new NotFoundException(`Chat Room with ID "${id}" not found`);
+    }
+    return chatRoom;
+  }
+
+  async deleteChatRoom(id: number, userId: number): Promise<void> {
+    const chatRoom = await this.getChatRoomById(id);
+    if (chatRoom.creator.id !== userId) {
+      throw new NotFoundException(
+        'You are not authorized to delete this chat room',
+      );
+    }
+    await this.chatRoomRepository.delete(id);
+  }
+}
