e9acd2490833a8d835422050306e589533ca5231
IFTEKHAR I ASIF <40823261+iftekhariasif@users.noreply.github.com>
2024-04-07 01:10:53 +0900

    Configured TypeORM module in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'chatadmin',
+      password: 'chatpassword',
+      database: 'hello-chat',
+      entities: [],
+      synchronize: true, // NOTE: Set to false in production
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
