3455f67ad880eca979ab048ac0bed081b982ecfb
IFTEKHAR I ASIF <40823261+iftekhariasif@users.noreply.github.com>
2024-04-07 05:47:51 +0900

    Added Functionalities to ChatRoom Controller

--- src/controllers/chat-rooms.controller.ts
+++ src/controllers/chat-rooms.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
+import { ChatRoomsService } from '../services/chat-rooms.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('chat-rooms')
-export class ChatRoomsController {}
+export class ChatRoomsController {
+  constructor(private readonly chatRoomsService: ChatRoomsService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  createChatRoom(@Body('name') name: string, @Request() req): Promise<any> {
+    return this.chatRoomsService.createChatRoom(name, req.user.userId);
+  }
+
+  @Get()
+  getAllChatRooms() {
+    return this.chatRoomsService.getAllChatRooms();
+  }
+
+  @Get(':id')
+  getChatRoomById(@Param('id') id: number) {
+    return this.chatRoomsService.getChatRoomById(id);
+  }
+
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  deleteChatRoom(@Param('id') id: number, @Request() req): Promise<void> {
+    return this.chatRoomsService.deleteChatRoom(id, req.user.userId);
+  }
+}
