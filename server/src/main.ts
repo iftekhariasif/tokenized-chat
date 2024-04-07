2754ea1e26bb9da6ab5ca63918c76b05c3e37b9e
IFTEKHAR I ASIF <40823261+iftekhariasif@users.noreply.github.com>
2024-04-07 21:18:34 +0900

    Fixed Corrs Error

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  // Enable CORS and restrict it to your React app's origin for added security
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
+  await app.listen(process.env.PORT || 4000); // Make sure to listen on port 4000 or another port different from React's
 }
 bootstrap();
